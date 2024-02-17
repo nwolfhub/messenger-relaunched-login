38764f3556e5afebd0b5db2af4e3e6ecf3d86b24
s0m31 <admin@nwolfhub.org>
2024-02-17 18:39:55 +0300

    Initial commit

---
+++ src/main/java/org/nwolfhub/messenger/login/LoginApplication.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.messenger.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
